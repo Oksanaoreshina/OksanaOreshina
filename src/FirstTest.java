8fa5f14ec2978f380f1dbdf6c02820a464cf1201
Oksana Oreshina <oksi.oreshiana@yandex.ru>
2023-07-08 20:14:37 +0300

    Урок 3. Задание1.
    3.Первые тесты на Android. Создание метода assertElementHasText

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -4,7 +4,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -32,8 +36,56 @@ public class FirstTest {
     @Test
     public void firstTest()
     {
-        System.out.println("First test run");
+        waitForElementByXPathAndClick(
+                "//*[contains(@text,'Skip')]",
+                "Cannot find Skip",
+                5
+        );
+
+        waitForElementByXPathAndClick(
+                "//*[contains(@text,'Search Wikipedia')]",
+                "Cannot find Search Wikipedia",
+                5
+        );
+
+        waitForElementByXPathAndSendKeys(
+                "//*[contains(@text,'Search Wikipedia')]",
+                "Cat",
+                "Cannot find Cat",
+                5
+        );
+
+        assertElementHasText(
+                "//*[@index = '2']//*[@text = 'British musician (born 1948)']",
+                "Cannot find 'British musician (born 1948)'",
+                15
+        );
+    }
+    private WebElement assertElementHasText(String xpath, String error_message, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
     }
 
+    private WebElement assertElementHasText(String xpath, String error_message)
+    {
+        return assertElementHasText(xpath, error_message, 5);
+    }
 
+    private WebElement waitForElementByXPathAndClick(String xpath, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = assertElementHasText(xpath, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+    private WebElement waitForElementByXPathAndSendKeys(String xpath, String value, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = assertElementHasText(xpath, error_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
 }
