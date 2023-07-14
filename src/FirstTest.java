042c2b117aa5f0e0939defdf1b29372e0ccec026
Oksana Oreshina <oksi.oreshiana@yandex.ru>
2023-07-14 03:54:17 +0300

    IV. Продвинутые тесты на Android.Ex5: Тест: сохранение двух статей
    4.Продвинутые тесты на Android.Ex5: Тест: сохранение двух статей

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,11 +1,13 @@
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -163,6 +165,197 @@ public class FirstTest {
 
     }
 
+    /*@Test
+    public void testSwipeArticle1()
+    {
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Skip')]"),
+                "Cannot find Skip",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Appium",
+                "Cannot find search input",
+                20
+        );
+
+        waitForElementAndClick(
+               By.xpath("//*[@class = 'android.widget.TextView'][@text = 'Appium']"),
+               "Cannot find 'Appium' topic searching by 'Appium'",
+              20
+        );
+
+        swipeUpToFindElement(
+                By.xpath("//*[@text = 'View article in browser']"),
+                "Cannot find the and of the article",
+                20
+
+        );
+    }*/
+
+    @Test
+    public void testSwipeArticle(){
+
+
+            waitForElementAndClick(
+                    By.xpath("//*[contains(@text,'Skip')]"),
+                    "Cannot find Skip",
+                    5
+            );
+
+            waitForElementAndClick(
+                    By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                    "Cannot find Search Wikipedia",
+                    5
+            );
+
+            waitForElementAndSendKeys(
+                    By.id("org.wikipedia:id/search_src_text"),
+                    "Java",
+                    "Cannot find search input",
+                    10
+            );
+
+            waitForElementAndClick(
+                    By.xpath("//*[@class = 'android.view.ViewGroup']//*[@text = 'Object-oriented programming language']"),
+                   "Cannot find 'Object-oriented programming language' topic searching by 'Java'",
+                   15
+            );
+
+        waitForElementAndClick(
+                By.xpath("//*[@class = 'android.view.View'][@content-desc = 'WORA']"),
+                "Cannot find option to add article to reading list",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@class = 'android.view.View'][@content-desc = 'WORA']"), //кликаю 2 раза на кнопку, потому что там разные варианты кнопок отображаются и я хочу второй вариант
+                "Cannot find option to add article to reading list",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text = 'Add to another reading list']"),
+                "Cannot find 'Add to another reading list'",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text = 'Create new']"),
+                "Cannot find 'Create new' reading list",
+                10
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[@text = 'Name of this list']"),
+                "My reading list",
+                "Cannot find 'Name of this list'",
+                10
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@class = 'android.widget.Button'][@text = 'OK']"),
+                "Cannot find option to add article to reading list - 'OK'",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@class = 'android.widget.ImageButton'][@content-desc = 'Navigate up']"), //назад
+                "Cannot find option to back",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_description'][@text = 'High-level programming language']"), //поиск второй статьи
+                "Cannot find 'High-level programming language' topic searching by 'Java'",
+                15
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/page_save"),      // вторая статья. нажать на Save
+                "Cannot find option to add article to reading list",
+                5
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/page_save"),
+                "Cannot find option to add article to reading list",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text = 'Add to another reading list']"),      //добавить в лист
+                "Cannot find 'Add to another reading list'",
+                15
+        );
+
+        waitForElementAndClick(              //добавить в лист me reading list
+                By.xpath("//*[@class = 'android.widget.TextView'][@text = 'My reading list']"),
+                "Cannot find option to add article to 'My reading list'",
+                5
+        );
+
+
+        waitForElementAndClick(
+                By.xpath("//*[@class = 'android.widget.ImageButton'][@content-desc = 'Navigate up']"), //назад в список статей
+                "Cannot find option to back",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@class = 'android.widget.ImageButton'][@content-desc = 'Navigate up']"), //назад в ленту, где есть кнопка со статьями
+                "Cannot find option to back",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@class = 'android.widget.ImageView'][@bounds = '[288,1596][360,1668]']"), //клик на переход к спискам листов
+                "Cannot find saved lists",
+                5
+        );
+        //свайп вниз до моего листа
+        swipeUpToFindElement(
+                By.xpath("//*[@text = 'My reading list']"),
+                "Cannot find my reading list",
+                20
+
+        );
+
+        waitForElementAndClick(  //клик на мой лист
+                By.xpath("//*[@text = 'My reading list']"),
+                "Cannot find the button Saved",
+                5
+        );
+        //удалить статью, смахнув влево
+
+        swipeElementToLeft(
+                By.xpath("//*[@text = 'Object-oriented programming language']"),
+                "Cannot swipe to left to delete"
+        );
+
+        waitForElementNotPresent(  //проверить, что статья удалена
+                By.xpath("//*[@text = 'Object-oriented programming language']"),
+                "Cannot swipe to left to delete",
+                5
+        );
+
+        waitForElementPresent( //проверить, что вторая статья осталась
+                By.xpath("//*[@text = 'High-level programming language']"),
+                "Cannot swipe to left to delete",
+                5
+        );
+
+        }
+
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(error_message + "\n");
@@ -193,6 +386,59 @@ public class FirstTest {
         return wait.until(
                 ExpectedConditions.invisibilityOfElementLocated(by)
         );
+    }
+
+    protected void swipeUp(int timeOfSwipe)
+    {
+      TouchAction action = new TouchAction(driver);
+      Dimension size = driver.manage().window().getSize();
+      int x = size.width / 2;
+      int start_y = (int)(size.height * 0.8);
+      int and_y = (int)(size.height * 0.2);
+      action
+              .press(x, start_y)
+              .waitAction(timeOfSwipe)
+              .moveTo(x, and_y)
+              .release()
+              .perform();
+    }
+
+    protected void swipeUpQuick()
+    {
+        swipeUp(200);
+    }
+
+    protected void swipeUpToFindElement(By by, String error_message, int max_swipes)
+    {
+        int already_swiped = 0;
+        while (driver.findElements(by).size() == 0)
+        if (already_swiped > max_swipes)
+        {
+            waitForElementPresent(by, "Cannot find element by swiping up. \n" + error_message, 0);
+            return;
+        }
+        {
+            swipeUpQuick();
+            ++already_swiped;
+        }
+    }
+
+    protected void swipeElementToLeft(By by, String error_message)
+    {
+      WebElement element = waitForElementPresent(by, error_message, 10);
+      int left_x = element.getLocation().getX();
+      int right_x = left_x + element.getSize().getWidth();
+      int upper_y = element.getLocation().getY();
+      int lower_y = upper_y + element.getSize().getHeight();
+      int middle_y = (upper_y + lower_y) / 2;
+
+        TouchAction action = new TouchAction(driver);
+        action
+                .press(right_x, middle_y)
+                .waitAction(150)
+                .moveTo(left_x, middle_y)
+                .release()
+                .perform();
     }
 
 }
